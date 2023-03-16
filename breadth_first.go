ac3c01e2ef4f98697a5b4025aa3f36f688f6e669
Ennis Lynch <ennis.lynch@gmail.com>
2023-03-16 14:57:51 -0400

    Add code to read in index file

--- breadth_first.go
+++ breadth_first.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"os"
+)
 
-//Main method
+// This method will panic on errors
+// just makes code a bit cleaner ...
+// hint, hint, hint
+func checkForErrorAndFail(someError error) {
+	if someError != nil {
+		panic(someError)
+	}
+}
+
+// Main method
 func main() {
-	fmt.Println("Hello World")
+	//Open our index file and do absolutely nothing with it!
+	var commonError error //use as common error variable
+	fileStream, commonError := os.Open("pelle.txt.csv")
+	checkForErrorAndFail(commonError)
+	fileStream.Close() //be good file stewards
 }
