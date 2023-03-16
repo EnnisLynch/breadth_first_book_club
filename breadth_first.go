63e43fb0072547aa62dbcf93187e49639142b59f
Ennis Lynch <ennis.lynch@gmail.com>
2023-03-16 15:00:55 -0400

    Added csv reading and output all index values

--- breadth_first.go
+++ breadth_first.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/csv"
+	"fmt"
 	"os"
+	"strconv"
 )
 
 // This method will panic on errors
@@ -19,5 +22,31 @@ func main() {
 	var commonError error //use as common error variable
 	fileStream, commonError := os.Open("pelle.txt.csv")
 	checkForErrorAndFail(commonError)
+
+	//use a CSV encoder to read the values
+	//requires import encoding/csv
+	lines, commonError := csv.NewReader(fileStream).ReadAll()
+	checkForErrorAndFail(commonError)
+
+	//Loop through the index file and read all
+	//of the index values and print
+	for index, record := range lines {
+		if index == 0 {
+			//skip the header line
+			continue //not required
+		} else {
+			start_index, commonError := strconv.ParseInt(record[0], 10, 64)
+			checkForErrorAndFail(commonError)
+
+			end_index, commonError := strconv.ParseInt(record[1], 10, 64)
+			checkForErrorAndFail(commonError)
+
+			//Print our start and end index to the
+			//output stream to see that we did
+			//actual work!
+			fmt.Println(start_index, end_index)
+		}
+	} //end lines
+
 	fileStream.Close() //be good file stewards
 }
